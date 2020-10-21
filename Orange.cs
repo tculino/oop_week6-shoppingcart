51b2949b2cfc46460200ddb2b68e1c349373d526
tculino <71397368+tculino@users.noreply.github.com>
2020-10-21 15:47:34 +0100

    Add files via upload

---
+++ Orange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6
+{
+    class Orange : Item
+    {
+        public Orange()
+            : base()
+        {
+            Cost = 0.25;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalCost = 0;
+            List<Item> cart = new List<Item>();
+            Orange a = new Orange();
+            cart.Add(a);
+            foreach (Item index in cart)
+            {
+                totalCost += index.Cost;
+            }
+            Console.WriteLine($"The total cost is : {totalCost}");
+        }
+    }
+}
