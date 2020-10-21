ba231028c1dc009127ca18cd1674bef41eedbc30
tculino <71397368+tculino@users.noreply.github.com>
2020-10-21 15:31:06 +0100

    Version1

---
+++ OOP6/OOP6/Apple.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6
+{
+    class Apple : Item
+    {
+        public Apple()
+            : base()
+        {
+            Cost = 0.6;
+        }
+    }
+}
---
+++ OOP6/OOP6/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6
+{
+    public abstract class Item
+    {
+        public double Cost { get; set; }
+        public Item() { }
+        public double Price() { return Cost; }
+    }
+}
